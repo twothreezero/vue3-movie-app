0775df9b9cd3f754d09d88083594857193057e4e
twothreezero <realtwothreezero@gmail.com>
2025-02-12 16:40:45 +0900

    Add movie search request

--- src/store/movie.js
+++ src/store/movie.js
@@ -27,16 +27,33 @@ export default {
   },
   // 비동기로 동작
   actions: {
-    async searchMovies({ commit }, payload) {
+    async searchMovies({ state, commit }, payload) {
       const { title, year, number, type } = payload
       const OMDB_API_KEY = 'bd51e689'
-      // const OMDB_API_KEY = '7035c60c'
 
       const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=1`)
       const { Search, totalResults } = res.data
       commit('updateState', {
         movies: Search,
       })
+      console.log(totalResults)
+      console.log(typeof totalResults)
+
+      const total = parseInt(totalResults, 10)
+      const pageLength = Math.ceil(total / 10)
+
+      if (pageLength > 1) {
+        for (let page = 2; page <= pageLength; page += 1) {
+          if (page > (number / 10)) {
+            break
+          }
+          const res = await axios.get(`https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`)
+          const { Search } = res.data
+          commit('updateState', {
+            movies: [...state.movies, ...Search]
+          })
+        }
+      }
     }
   },
 }
