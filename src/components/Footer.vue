a3a5383595eac3981341187b9b30ba1b710f5f23
twothreezero <realtwothreezero@gmail.com>
2025-02-21 19:06:00 +0900

    Add footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,33 @@
+<template>
+  <footer>
+    <Logo />
+    <a href="https://github.com/twothreezero" target="_blank">
+      (c){{ new Date().getFullYear() }} twothreezero
+    </a>
+  </footer>
+</template>
+
+<script>
+import Logo from '~/components/Logo'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '~/scss/main';
+
+footer {
+  padding: 70px 0;
+  text-align: center;
+  opacity: .3;
+
+  .logo {
+    display: block;
+    margin-bottom: 4px;
+  }
+}
+</style>
