38e150fba14db55c93e7f5c3eadaea43aa804cfc
twothreezero <realtwothreezero@gmail.com>
2025-02-27 03:52:54 +0900

    Style as about

---
+++ src/store/about.js
@@ -0,0 +1,14 @@
+import pic from '../assets/logo.png'
+
+export default {
+  namespaced: true,
+  state: () => {
+    return {
+      name: 'twothreezero',
+      email: 'realtwothreezero@gmail.com',
+      blog: 'realtwothreezero.com',
+      phone: '+82-10-8380-6302',
+      image: pic
+    }
+  }
+}
