f0e42bcc7e4c6ae30f17d54d6b5fb2812579b21e
twothreezero <realtwothreezero@gmail.com>
2025-02-28 02:46:46 +0900

    modify: server port nubmer

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack')
 const HtmlPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const { VueLoaderPlugin } = require('vue-loader')
-const Dotenv = createRequire('dotenv-webpack')
+const Dotenv = require('dotenv-webpack')
 
 module.exports = {
   resolve: {
@@ -87,7 +87,7 @@ module.exports = {
   // 개발 서버 옵션
   devServer: {
     host: 'localhost',
-    port: 8080,
+    port: 8079,
     hot: true
   }
 }
