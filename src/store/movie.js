b0e3c6573e335d8e0c3807a92ab1452708738560
twothreezero <realtwothreezero@gmail.com>
2025-02-28 02:26:16 +0900

    Add: movie fucntion API for netlify

--- src/store/movie.js
+++ src/store/movie.js
@@ -42,7 +42,6 @@ export default {
         const { Search, totalResults } = res.data
         commit('updateState', {
           movies: _uniqBy(Search, 'imdbID'),
-          // movies: Search,
         })
 
         console.log(Search)
@@ -70,10 +69,10 @@ export default {
             })
           }
         }
-      } catch (message) {
+      } catch ({ message }) {
         commit('updateState', {
           movies: [],
-          message: message
+          message
         })
       } finally {
         commit('updateState', {
@@ -107,23 +106,6 @@ export default {
   },
 }
 
-function _fetchMovie(payload) {
-  const { title, type, year, page, id } = payload
-  const OMDB_API_KEY = 'bd51e689'
-  const url = id
-    ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}`
-    : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
-
-  return new Promise((resolve, reject) => {
-    axios.get(url)
-      .then((res) => {
-        if (res.data.Error) {
-          reject(res.data.Error)
-        }
-        resolve(res)
-      })
-      .catch(err => {
-        reject(err.message)
-      })
-  })
+async function _fetchMovie(payload) {
+  return await axios.post('/.netlify/functions/movie', payload)
 }
